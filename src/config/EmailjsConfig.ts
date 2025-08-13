cff456606701a32c702747addc339ae850b27f00
Ilir-Prog <iliri.isufi@gmail.com>
2025-08-13 16:04:58 +0200

    Updated EmailjsConfig.ts

--- src/config/EmailjsConfig.ts
+++ src/config/EmailjsConfig.ts
@@ -1,5 +1,5 @@
 export const emailjsConfig = {
-  PUBLIC_KEY: "YOUR_PUBLIC_KEY",
-  TEMPLATE_ID: "YOUR_TEMPLATE_ID", 
-  SERVICE_ID: "YOUR_SERVICE_ID"
+  PUBLIC_KEY: "OY-V8nlBsSjKT_AKQ",
+  TEMPLATE_ID: "template_o0y2nms", 
+  SERVICE_ID: "service_xt3n1ig"
 };
